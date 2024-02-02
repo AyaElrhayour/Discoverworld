3ceba272165f761cf403c8485af9fc36bdf5f52c
Aya <ayaelrhayour123@gmail.com>
2024-02-02 19:51:23 +0100

    partials

---
+++ resources/views/partials/_hero.blade.php
@@ -0,0 +1,23 @@
+<section class="relative h-72  flex flex-col justify-center align-center text-center space-y-4 mb-4">
+    <div class="absolute  left-0 w-full bg-no-repeat bg-center  top-[-60px] h-[31rem] opacity-90 z-[-1]"
+        style="background-image: url('images/bgo4.jpg')"></div>
+    <div class="z-10">
+        <h1 class="text-6xl font-bold uppercase text-white">
+            Adventure<span class="text-orange-500"> Time</span>
+        </h1>
+        <p class="text-2xl text-gray-200 font-bold my-4">
+            Unite, Explore, Thrive: Where Every Journey Is an Adventure!
+        </p>
+        <div>
+            @auth
+                <a href="/adventure/create"
+                    class="inline-block border-2 border-orange-500 text-orange-500 py-2 px-4 rounded-xl uppercase mt-2 hover:text-white hover:border-white">Share</a>
+            @else
+                <a href="/register"
+                    class="inline-block border-2 border-white text-white py-2 px-4 rounded-xl uppercase mt-2 hover:text-black hover:border-black">Sign
+                    Up to List
+                    a Gig</a>
+            @endauth
+        </div>
+    </div>
+</section>
---
+++ resources/views/partials/_search.blade.php
@@ -0,0 +1,14 @@
+<form action="">
+    <div class="relative border-2 border-gray-100 mt-48 mb-20 m-4 rounded-lg">
+        <div class="absolute top-4 left-3">
+            
+        </div>
+        <input type="text" name="search" class="h-14 w-full pl-10 pr-20 rounded-[18.5rem] z-0 focus:shadow focus:outline-none"
+            placeholder="Search For Your New Destination..." />
+        <div class="absolute top-2 right-2">
+            <button type="submit" class="h-10 w-20 text-white rounded-[18.5rem] bg-orange-500 hover:text-orange-500">
+                Search
+            </button>
+        </div>
+    </div>
+</form>
